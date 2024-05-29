ed9b63f55a5b5327054d798bb7e109e839f65d69
yeralShaik <yeraldinshaik@gmail.com>
2024-05-29 12:41:20 -0500

    [FIX] version en ingles y link de cv

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const langDataEnglish = {
-        
+        changeLanguage: 'Spanish',
         name: '¡Hello! I\'m Yeraldin Espinosa',
         ocupation: 'FrontEnd Developer',
         location: '📍Panamá, Panamá City',
         aboutme: 'About me',
+        downloadCV: 'Download CV',
         text: 'With a solid background in sales, I decided to pivot my career and dive into the fascinating world of web development, with a special focus on Frontend.<br> I am a self-taught individual with a strong capacity for learning and adapting to new environments.<br> I have a passion for researching, experimenting, and applying my knowledge to achieve my goals.<br> I have developed various personal projects that have allowed me to practice my skills and explore new web development techniques.<br> I consider myself a curious person with a great appetite for knowledge. I am always seeking new opportunities to learn and improve my skills.',
-        tooltitle: 'Skills',
         projecttitle: 'Projects',
-        projectDescription1: 'This project involves the creation of a Landing Page for Shaik Asesores, a Panamanian company dedicated to providing advice on personal loans.',
-        projectDescription2: 'MovieStream is a web application that allows you to discover and explore movies from various categories.',
-        projectDescription3: "Gif App is a simple app to search and display gifs using the Giphy API.",
-        projectDescription4: 'The challenge of this project was to design an application that addressed the needs of those looking to adopt pets or find lost ones.',
-        changeLanguage: 'Spanish'
+        description: 'Recipe page, created from scratch using Figma, HTML, and CSS.',
+        tooltitle: 'Skills',
+        viewProjectlink: 'View project'
+
+
+        
+        
+
     }
   
     const langDataSpanish = {
@@ -21,50 +24,58 @@ document.addEventListener('DOMContentLoaded', function () {
         ocupation: 'Desarrolladora FrontEnd 💻',
         location: '📍Panamá, Ciudad de Panamá',
         aboutme: 'Sobre mí',
+        downloadCV: 'Descargar CV',
         text: 'Con una sólida experiencia en el mundo de las ventas, decidí dar un giro a mi carrera y sumergirme en el fascinante universo del desarrollo web, enfocándome especialmente en el Frontend.<br>Soy una persona autodidacta, con gran capacidad de aprendizaje y adaptación a nuevos entornos.<br>Me apasiona investigar, experimentar y poner en práctica mis conocimientos para alcanzar mis objetivos.<br>He desarrollado diversos proyectos personales que me han permitido poner en práctica mis habilidades y explorar nuevas técnicas de desarrollo web.<br>Me considero una persona curiosa y con un gran apetito por el conocimiento. Siempre estoy buscando nuevas oportunidades para aprender y mejorar mis habilidades.',
         tooltitle: 'Skills',
         projecttitle: 'Proyectos',
-        changeLanguage: 'English'
+        changeLanguage: 'English',
+
+
     }
   
     let currentLanguage = 'spanish'
   
     function updateLanguage() {
         const langData = currentLanguage === 'english' ? langDataEnglish : langDataSpanish
-        document.getElementById('changeLanguage').textContent = langData.changeLanguage
-        
-        document.getElementById('name').textContent = langData.name
-        document.getElementById('ocupation').textContent = langData.ocupation
+        document.querySelector('#changeLanguage').textContent = langData.changeLanguage
+        document.querySelector('#name').textContent = langData.name
+        document.querySelector('#ocupation').textContent = langData.ocupation
+        document.querySelector('#location').textContent = langData.location
         document.querySelector('.aboutme-title').textContent = langData.aboutme
-        document.getElementById('location').textContent = langData.location
-        document.getElementById('text').innerHTML = langData.text
-        document.getElementById('tooltitle').textContent = langData.tooltitle
+        document.querySelector('#cv-download').textContent = langData.downloadCV
+        document.querySelector('#text').innerHTML = langData.text
+        document.querySelector('.project-title').textContent = langData.projecttitle
+        document.querySelector('#tooltitle').textContent = langData.tooltitle
+        document.querySelector('.project-Description').textContent = langData.description
+        document.querySelector('.link').textContent = langData.viewProjectlink
+    
     }
   
-    document.getElementById('changeLanguage').addEventListener('click', () => {
+    document.querySelector('#changeLanguage').addEventListener('click', () => {
         currentLanguage = currentLanguage === 'english' ? 'spanish' : 'english'
         updateLanguage()
     })
-  
+
+    const phoneNumber = '+50760745606'
+    const message = 'Hola, estoy interesado en contactarte.'
+    const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`
+    const linkedinUrl = 'https://www.linkedin.com/in/yeraldinespinosa/'
+    const gitHubUrl = 'https://github.com/YeralShaik'
+    const mailUrl = 'mailto:yeraldinshaik@gmailcom'
+    
     document.getElementById('whatsapp-chat').addEventListener('click', function () {
-        const phoneNumber = '+50760745606'
-        const message = 'Hola, estoy interesado en contactarte.'
-        const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`
         window.open(whatsappURL, '_blank')
     })
   
     document.querySelector('.linkedin-button').addEventListener('click', function () {
-        const linkedinUrl = 'https://www.linkedin.com/in/yeraldinespinosa/'
         window.open(linkedinUrl, '_blank')
     })
   
     document.querySelector('.github-button').addEventListener('click', function () {
-        const gitHubUrl = 'https://github.com/YeralShaik'
         window.open(gitHubUrl, '_blank')
     })
   
-    document.querySelector('.mail-button').addEventListener('click', function () {
-        const mailUrl = 'mailto:yeraldinshaik@gmailcom'
+    document.querySelector('.mail-button').addEventListener('click', function () { 
         window.open(mailUrl, '_blank')
     })
   
@@ -93,38 +104,39 @@ document.addEventListener('DOMContentLoaded', function () {
   
     projectsData.forEach((project) => {
         const projectCard = document.createElement('div')
-        projectCard.className = 'project-Card'
-  
         const projectImg = document.createElement('img')
-        projectImg.src = project.imgSrc
-        projectImg.alt = `Imagen del proyecto ${project.title}`
         const projectImgWrapper = document.createElement('div')
+        const projectContent = document.createElement('div')
+        const projectName = document.createElement('h2')
+        const projectDescription = document.createElement('p')
+        const linksContainer = document.createElement('div')
+        const githubLink = document.createElement('a')
+        const projectImgLink = document.createElement('a')
+
+        projectCard.className = 'project-Card'
+
+        projectImg.src = project.imgSrc
+        projectImg.alt = `Imagen del proyecto ${project.title}`   
         projectImgWrapper.className = 'project-Img'
         projectImgWrapper.appendChild(projectImg)
-  
-        const projectContent = document.createElement('div')
+
         projectContent.className = 'project-Content'
-  
-        const projectName = document.createElement('h2')
         projectName.className = 'project-Name'
         projectName.innerText = project.title
         projectContent.appendChild(projectName)
-  
-        const projectDescription = document.createElement('p')
+
         projectDescription.className = 'project-Description'
         projectDescription.innerText = project.description
         projectContent.appendChild(projectDescription)
-  
-        const linksContainer = document.createElement('div')
+        
+    
+
         linksContainer.className = 'links-Container'
-  
-        const githubLink = document.createElement('a')
         githubLink.href = project.githubLink
         githubLink.className = 'github-link'
         githubLink.innerText = 'Code'
         linksContainer.appendChild(githubLink)
   
-        const projectImgLink = document.createElement('a')
         projectImgLink.href = project.link
         projectImgLink.className = 'link'
         projectImgLink.innerText = 'Ver proyecto'
